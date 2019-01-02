8c088e191e2f3f0ad0322021a036cb40f37e4091
Sersho <Sersho@GameFarming.ru>
2019-01-02 13:41:22 +0300

    Second

--- ClickermannHelper/Program.cs
+++ ClickermannHelper/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace ClickermannHelper
+﻿using System;
+
+namespace ClickermannHelper
 {
     class Program
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Hello, GitHub!");
+            Console.ReadLine();
         }
     }
 }
